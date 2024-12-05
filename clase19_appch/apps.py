09e2d19a4fc5beafbd1cb999ba571983234b779f
Arturo Lopez <arturo.lopezjg@gmail.com>
2024-12-05 14:46:10 -0600

    First commit

---
+++ clase19_appch/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Clase19AppchConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'clase19_appch'
---
+++ clase19_appch/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from clase19_appch.views import inicio
+
+urlpatterns = [
+    path('Portada/', inicio),
+]
